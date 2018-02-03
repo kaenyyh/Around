39f6d68b07b87c0e008ec1bc1ed1dcf038f77343
kaenyyh <kaenyyh@gmail.com>
2018-02-03 17:22:53 -0600

    first simple example of request responder

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+)
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
+}
+
+
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received a request")
+	decoder := json.NewDecoder(r.Body)
+
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)
+		return
+	}
+	fmt.Fprintf(w, "Post is received : %s\n", p.Message)
 }
